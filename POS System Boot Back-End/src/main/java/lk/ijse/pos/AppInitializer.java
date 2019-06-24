b344819a6c83c11c8c07a880674bc62a76b480e3
Pramith <aravindapramith3@gmail.com>
2019-06-24 09:41:39 +0530

    Initializing

---
+++ POS System Boot Back-End/src/main/java/lk/ijse/pos/AppInitializer.java
@@ -0,0 +1,15 @@
+package lk.ijse.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppInitializer {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AppInitializer.class, args);
+
+    }
+
+}
